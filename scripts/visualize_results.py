7ace97c028a780f9cbf7a16b0743155da4216280
Lukas Fröhlich <lukasfro@ethz.ch>
2021-07-23 11:56:30 +0200

    Update visualization script

--- scripts/visualize_results.py
+++ scripts/visualize_results.py
@@ -1,21 +1,24 @@
 import argparse
-import GPy
 import json
+from botorch.models.transforms.input import Normalize
+from botorch.models.transforms.outcome import Standardize
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import yaml
 
+from botorch.models import SingleTaskGP
+import torch
+
 
 def show_convergence(log_dir, axis):
     eval_dict = load_eval(log_dir)
 
-    eval_dict = {k: np.array(v) for k, v in eval_dict.items()}
-    num_evals = eval_dict["y_eval"].shape[0]
+    num_evals = eval_dict["train_targets"].shape[0]
     eval_iter = np.arange(num_evals) + 1
 
     axis.plot(eval_iter, eval_dict["y_best"], "C3", label="Best function value")
-    axis.plot(eval_iter, eval_dict["y_eval"], "ko", label="Function value")
+    axis.plot(eval_iter, eval_dict["train_targets"], "ko", label="Function value")
     axis.set_xlabel("# Iteration")
     axis.set_ylabel("Function value")
     axis.legend()
@@ -28,57 +31,53 @@ def show_model(log_dir, axis):
     eval_dict = load_eval(log_dir)
 
     # Visualization depends on dimensionality of the input space
-    if config["input_dim"] == 1:
-        show_model_1d(gp, config, eval_dict, axis)
-    elif config["input_dim"] == 2:
-        show_model_2d(gp, config, eval_dict, axis)
-    else:
-        print("Only 1- and 2-dimensional models can be plotted.")
-        return
-
-
-def show_model_1d(gp, config, eval_dict, axis):
+    assert config["input_dim"] == 1
 
     lb, ub = config["lower_bound"][0], config["upper_bound"][0]
-    x_plot = np.linspace(lb, ub, 500)
-    mean, var = gp.predict(x_plot[:, None])
-    mean = mean.squeeze()
-    std = np.sqrt(var).squeeze()
+    x_plot = torch.linspace(lb, ub, 500)
+    posterior = gp.posterior(x_plot.unsqueeze(-1))
+    mean = posterior.mean.squeeze().detach()
+    upper, lower = posterior.mvn.confidence_region()
+
+    # mean, var = gp.posterior.mean(x_plot)
+    # mean = mean.squeeze()
+    # std = np.sqrt(var).squeeze()
 
     axis.plot(x_plot, mean, label="GP mean")
-    axis.fill_between(
-        x_plot, mean + 2 * std, mean - std * std, label="95% confidence", alpha=0.3
-    )
-    axis.plot(eval_dict["x_eval"], eval_dict["y_eval"], "ko", label="Evaluated points")
-    axis.plot(
-        eval_dict["x_best"][-1], eval_dict["y_best"][-1], "C8*", label="Best point"
-    )
+    axis.fill_between(x_plot, upper.detach(), lower.detach(), label="95% confidence", alpha=0.3)
+    axis.plot(eval_dict["train_inputs"], eval_dict["train_targets"], "ko", label="Data")
+    axis.plot(eval_dict["x_best"][-1], eval_dict["y_best"][-1], "C8*", label="Best point")
     axis.set_xlabel("Optimzation parameter")
     axis.set_ylabel("Function value")
     axis.legend()
 
 
-def show_model_2d(gp, config, eval_dict, axis):
-    lb, ub = config["lower_bound"], config["upper_bound"]
-    x_eval = np.array(eval_dict["x_eval"])
-    x_best = np.array(eval_dict["x_best"])[-1]
-    y_best = np.array(eval_dict["y_best"])[-1]
-
-    print(x_eval)
-    axis.plot([lb[0], ub[0], ub[0], lb[0], lb[0]],
-              [lb[1], lb[1], ub[1], ub[1], lb[1]])
-    axis.plot(x_eval[:, 0], x_eval[:, 1], "kx")
-    axis.plot(x_best[0], x_best[1], "ro")
-    
-    print(x_best, y_best)
-    
-
 def load_gp(log_dir):
     try:
-        model_file = os.path.join(log_dir, "model.json")
-        with open(model_file, "r") as f:
-            model_dict = json.load(f)
-            gp = GPy.models.GPRegression._from_dict(model_dict)
+        # The GP state, i.e., hyperparameters, normalization, etc.
+        model_file = os.path.join(log_dir, "model_state.pth")
+        with open(model_file, "rb") as f:
+            state_dict = torch.load(f)
+
+        # Get the evaluated data points
+        eval_dict = load_eval(log_dir)
+        train_X = eval_dict["train_inputs"]
+        train_Y = eval_dict["train_targets"]
+
+        # The bounds of the domain
+        config_dict = load_config(log_dir)
+        lb = torch.tensor(config_dict["lower_bound"])
+        ub = torch.tensor(config_dict["upper_bound"])
+        bounds = torch.stack((lb, ub))
+
+        # Create GP instance and load respective parameters
+        gp = SingleTaskGP(
+            train_X=train_X,
+            train_Y=train_Y,
+            outcome_transform=Standardize(m=1),
+            input_transform=Normalize(d=1, bounds=bounds),
+        )
+        gp.load_state_dict(state_dict=state_dict)
     except FileNotFoundError:
         print(f"The model file could not be found in: {log_dir}")
         exit(1)
@@ -90,7 +89,6 @@ def load_config(log_dir):
         config_file = os.path.join(log_dir, "config.yaml")
         with open(config_file, "r") as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
-            print(config)
     except FileNotFoundError:
         print(f"The config file could not be found in: {log_dir}")
         exit(1)
@@ -102,6 +100,7 @@ def load_eval(log_dir):
         evaluations_file = os.path.join(log_dir, "evaluations.yaml")
         with open(evaluations_file, "r") as f:
             eval_dict = yaml.load(f, Loader=yaml.FullLoader)
+        eval_dict = {k: torch.tensor(v) for k, v in eval_dict.items()}
     except FileNotFoundError:
         print(f"The evaluations file could not be found in: {log_dir}")
         exit(1)
@@ -120,11 +119,12 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
     fig.suptitle(f"BayesOpt4ROS \n Logging directory: {args.log_dir}")
+    gp = load_gp(args.log_dir)
+
     show_convergence(args.log_dir, axes[0])
     show_model(args.log_dir, axes[1])
     plt.tight_layout()
-    plt.savefig(os.path.join(args.log_dir, "results.png"))
-    plt.show()
+    plt.savefig(os.path.join(args.log_dir, "final_visualization.png"))
+    # plt.show()
